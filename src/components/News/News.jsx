77e552197df8f71bede94c40470249a76ff7cb15
MehediHasan95 <imehedihasan95@gmail.com>
2023-05-02 09:45:04 +0600

    fontawsome added

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -5,8 +5,13 @@ const News = ({ news }) => {
   console.log(news);
   const { banner, profile, author, create, read, title, hash } = news;
   return (
-    <div>
-      <h1>News</h1>
+    <div className="news-card">
+      <div className="banner">
+        <img src={banner} alt="" />
+      </div>
+      <div className="details">
+        <div className="profile"></div>
+      </div>
     </div>
   );
 };
